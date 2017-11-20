7bbd9b220f6257cf0636bcf65226f6f877bb48e3
Ashly Berry <ashlyberry@Ymail.com>
2017-11-20 00:33:43 -0700

    updated layout with rounded corners temp colors

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 $( document ).ready(function() {
    var gameBoard = $('.gameboard');
    var header = $(".header");
- 
-
+   var card = $(".card");
+   
+   card.click(function() {
+	  //$(this).css("background-color", "red"); 
+	  
+	  $(this).css("visibility", "hidden");
+   });
+   
+   
+   
 });
